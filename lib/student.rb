967f564c90aa50324d9c5b32ebbc1de76f281a17
Jason Viola <jason.viola@mavs.uta.edu>
2020-06-09 18:10:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,6 +7,11 @@ class Student
   
   def self.create_table
     sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS 
+    
+    
+    
+    
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
