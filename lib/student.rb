4ab58ff24c458b559877ec80817921844497a001
Jason Viola <jason.viola@mavs.uta.edu>
2020-06-09 18:11:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,10 +8,11 @@ class Student
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
-    id INTEGER PRIMARY KEY
-    name TEXT
-    grade INTEGER
-    );
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade INTEGER,
+    )
+    SQL
     
     
     
