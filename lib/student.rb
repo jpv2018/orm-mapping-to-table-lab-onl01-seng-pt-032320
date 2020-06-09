0df135de79c495c638e684c8146caaaa1f74b32b
Jason Viola <jason.viola@mavs.uta.edu>
2020-06-09 18:23:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,7 @@ class Student
     VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT LAST ID")
+    @id = DB[:conn].execute("SELECT last_insert_row_id")
   end
   
   def self.create(name, grade)
