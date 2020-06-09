a0304e23488c1a45234fa1f5a97980fbe531621f
Jason Viola <jason.viola@mavs.uta.edu>
2020-06-09 18:09:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,8 @@ class Student
     @id = id
   end
   
-  def create_table
+  def self.create_table
+    
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
