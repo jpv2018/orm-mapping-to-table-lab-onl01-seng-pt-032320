4b2b4e70dfdead8422d04456dd272647698a176d
Jason Viola <jason.viola@mavs.uta.edu>
2020-06-09 18:09:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
     @name = name
     @grade = grade
     @id = id
-    
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
